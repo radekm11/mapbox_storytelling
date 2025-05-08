2611d14be81b6654ac2c7289a04942a2d8a82f45
radekm11 <radekmagulick@gmail.com>
2025-05-07 22:50:31 -0400

    adding 5 more slides attempt

--- config.js
+++ config.js
@@ -143,6 +143,130 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
+         },
+{
+            id: 'opening',
+            alignment: 'centered',
+            hidden: false,
+            title: 'Mapping Gender and Economic Power in Southern Literature',
+            image: 'C:\Users\radek\OneDrive\Desktop\SL final title image.jpeg',
+            description: 'This project maps purchases from The Color Purple, Salvage the Bones, and As I Lay Dying. Male and female purchases are represented using different colors for each book. The data highlights the gendered economic activity in these Southern novels and explores how money and power interact.',
+            location: {
+                center: [-90.3150946, 38.6605671],
+                zoom: 4,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'chapter-one',
+            alignment: 'right',
+            hidden: false,
+            title: 'Someplace Georgia',
+            image: './assets/alice_walker_childhood_home.jpg',
+            description: 'Description of the second location.',
+            location: {
+                center: [-83.5573651, 32.5573651], 
+                zoom: 6.5,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'opening',
+            alignment: 'centered',
+            hidden: false,
+            title: 'The Color Purple',
+            image: './assets/color_purple_cover.jpg',
+            description: 'This is where I insert a description of the novel',
+            location: {
+                center: [-90.3150946, 38.6605671],
+                zoom: 4,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+        {
+            id: 'chapter-two',
+            alignment: 'left',
+            hidden: false,
+            title: 'Olinka',
+            image: './assets/firestone_farm.jpg',
+            description: 'Description of the Olinka people',
+            location: {
+                center: [-10.378889436404226, 6.251275915595865], 
+                zoom: 8.00,
+                pitch: 45.0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'chapter-three',
+            alignment: 'fully',
+            hidden: false,
+            title: 'End',
+            image: './assets/color_purple_field.jpg',
+            description: 'Close of the novel.',
+            location: {
+                center: [-41.47212410646295, 34.781162343274076], 
+                zoom: 3,
+                pitch: 30,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
         }
     ]
 };
